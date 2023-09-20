b118b65f373d6ce3b4bf538394f1ee0fdaac1246
panan183panan <3234059020@qq.com>
2023-09-20 17:22:15 +0800

    添加一键登录相关配置

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -8,9 +8,35 @@ export default function Login(props: any) {
     console.log(values);
   };
 
+  const loginData = {
+    url: 'http://172.19.80.76:5090/passport/index.php',
+    appid: '6TkC2zS1Q238aXfTLEYy',
+    secret: 'wklgM9HtPAzRy8Wza66B4WJT3lhsvJKoXNxL53t8Hj09ii8f64JKYjkT4Qxh',
+  };
+
+  const quickLogin = () => {
+    const loginPlatformState = Date.now().toString();
+    window.location.href = `${loginData.url}?state=${loginPlatformState}&appid=${loginData.appid}`;
+  };
+
+  const typeBg = [
+    'color',
+    'ball',
+    'lines',
+    'thick',
+    'circle',
+    'cobweb',
+    'polygon',
+    'square',
+    'tadpole',
+    'fountain',
+    'random',
+    'custom',
+  ];
+
   return (
     <div style={{ height: '100%', overflow: 'hidden' }}>
-      <ParticlesBg type="lines" bg={true} />
+      <ParticlesBg type={typeBg[Math.floor(Math.random() * 12)]} bg={true} />
       {/* 这里的type的属性值可以手动更改，可以是random，circle，lines等等*/}
       <div className="formContainer">
         <div className="logintitle">股票代码组合系统</div>
@@ -46,7 +72,11 @@ export default function Login(props: any) {
                 </Button>
               </Col>
               <Col>
-                <Button type="link" className="login-form-button">
+                <Button
+                  type="link"
+                  className="login-form-button"
+                  onClick={quickLogin}
+                >
                   一键登录
                 </Button>
               </Col>
