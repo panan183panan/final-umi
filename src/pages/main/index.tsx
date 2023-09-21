34ec7e5fffa0d71c02812929219c7bdba6b7c004
panan183panan <3234059020@qq.com>
2023-09-21 16:22:27 +0800

    一键登录完善,代码合并

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -3,7 +3,7 @@ import isAdministrator from '@/utils/administrator';
 export default function IndexPage() {
   return (
     <div>
-      欢迎您，
+      <span>欢迎您:</span>
       {isAdministrator(sessionStorage.getItem('email') || '')
         ? '管理员'
         : '普通用户'}
